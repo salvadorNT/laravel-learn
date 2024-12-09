e77307ea74e06f512587a9a0a45ef2f3e678e914
salvador <salvadorjnt@gmail.com>
2024-12-09 00:12:19 -0600

    View: created Post index view

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index</title>
 </head>
+
 <body>
-    
+    <ul>
+        @foreach ($posts as $post)
+            <li>Title: {{ $post->title }}
+                <ul>
+                    <li>ID: {{ $post->id }}</li>
+                    <li>Description: {{ $post->description }}</li>
+                    <li>Category: {{ $post->category->title }}</li>
+                    <li>Posted: {{ $post->posted }}</li>
+                </ul>
+            </li>
+        @endforeach
+    </ul>
 </body>
+
 </html>
