31b86956963ad09543a2d38cde44dd354c53d579
salvador <salvadorjnt@gmail.com>
2024-12-08 20:02:28 -0600

    Migrate: added column title to categories

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
-{
+{  
     use HasFactory, Notifiable;
 
     /**
