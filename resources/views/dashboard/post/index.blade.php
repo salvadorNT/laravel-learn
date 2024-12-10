8333376846495c04ae8188536cd554dde34c676c
salvador <salvadorjnt@gmail.com>
2024-12-09 22:50:53 -0600

    View: moved index from post to post folder and inherit from main template

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('dashboard.template_main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
-</head>
-
-<body>
+@section('content')
     <ul>
         @foreach ($posts as $post)
             <li>Title: {{ $post->title }}
@@ -21,6 +13,4 @@
             </li>
         @endforeach
     </ul>
-</body>
-
-</html>
+@endsection
