4f72a1d48bf1e3548dfd572199600d8a6d4d86ab
salvador <salvadorjnt@gmail.com>
2024-12-09 22:51:27 -0600

    View: created form to submit a Post

---
+++ resources/views/dashboard/post/create.blade.php
@@ -0,0 +1,45 @@
+@extends('dashboard.template_main')
+
+@section('title')
+    Create Post
+@endsection
+
+@section('content')
+    <form action="{{ route('post.store') }}" method="post">
+        
+        @csrf
+
+        <label for="title">Title</label>
+        <input type="text" name="title" id="" required>
+
+        <label for="slug">Slug</label>
+        <input type="text" name="slug" id="">
+
+        <label for="description">Description</label>
+        <textarea name="description" id="" cols="30" rows="10"></textarea>
+        
+
+        <label for="content">Content</label>
+        <input type="textarea" name="content" id="">
+
+        <label for="category_id">Category</label>
+        <select name="category_id" id="">
+            @foreach ($categories as $category)
+                <option value={{ $category->id }}>{{ $category->title }}</option>
+            @endforeach
+        </select>
+
+        <label for="image">Image</label>
+        <input type="textarea" name="image" id="">
+
+        <label for="posted">Posted</label>
+        <select name="posted" id="">
+            <option value="yes">Yes</option>
+            <option value="no" selected>No</option>
+        </select>
+
+
+
+        <button type="submit">Guardar</button>
+    </form>
+@endsection
