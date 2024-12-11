db54ece1a7c01f8ed678a8938544fe1fd8cdacba
salvador <salvadorjnt@gmail.com>
2024-12-10 22:46:56 -0600

    modified index display list to table

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,16 +1,18 @@
 @extends('dashboard.template_main')
 
 @section('content')
-    <ul>
-        @foreach ($posts as $post)
-            <li>Title: {{ $post->title }}
-                <ul>
-                    <li>ID: {{ $post->id }}</li>
-                    <li>Description: {{ $post->description }}</li>
-                    <li>Category: {{ $post->category->title }}</li>
-                    <li>Posted: {{ $post->posted }}</li>
-                </ul>
-            </li>
-        @endforeach
-    </ul>
+<table>
+    <th>ID</th>
+    <th>Title</th>
+    <th>Category</th>
+    <th>Posted</th>
+    @foreach ($posts as $post)
+    <tr>
+        <td>{{ $post->id }}</td>
+        <td>{{ $post->description }}</td>
+        <td>{{ $post->category->title}}</td>
+        <td>{{ $post->posted }}</td>
+    </tr>
+    @endforeach
+</table>
 @endsection
