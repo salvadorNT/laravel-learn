66d1c702585de56c9201ea5f2a5904830ec363a8
salvador <salvadorjnt@gmail.com>
2024-12-10 23:33:15 -0600

    Added actions to posts index table

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,20 +1,30 @@
 @extends('dashboard.template_main')
 
 @section('content')
-<table>
-    <th>ID</th>
-    <th>Title</th>
-    <th>Category</th>
-    <th>Posted</th>
-    @foreach ($posts as $post)
-    <tr>
-        <td>{{ $post->id }}</td>
-        <td>{{ $post->description }}</td>
-        <td>{{ $post->category->title}}</td>
-        <td>{{ $post->posted }}</td>
-    </tr>
-    @endforeach
-</table>
+    <a href="{{ route('post.create') }}">
+        <button>Crear</button>
+    </a>
 
-{{$posts->links()}}
+    <table>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Category</th>
+        <th>Posted</th>
+        <th>Options</th>
+        @foreach ($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->description }}</td>
+                <td>{{ $post->category->title }}</td>
+                <td>{{ $post->posted }}</td>
+                <td>
+                    <a href="{{ route('post.edit', $post->id) }}">Editar</a>
+                    <a href="{{ route('post.show', $post->id) }}">Detalle</a>
+                    <a href="{{ route('post.destroy', $post->id) }}">Eliminar</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+    {{ $posts->links() }}
 @endsection
